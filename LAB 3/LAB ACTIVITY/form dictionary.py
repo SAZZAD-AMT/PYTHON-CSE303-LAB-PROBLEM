daf81def9a8afbc7c629d62c8c078f7e576ad2c5
Sazzad Hossen <80639629+SAZZAD-AMT@users.noreply.github.com>
2022-03-02 00:43:06 +0600

    Add files via upload

---
+++ LAB 3/LAB ACTIVITY/form dictionary.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+dict1 = {'id':[1,2,3],'name':['alice','bob','charlie'],
+ 'age':[20, 25, 32]}
+df1 = pd.DataFrame(dict1)
+print(df1)
---
+++ LAB 3/LAB ACTIVITY/formlist.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+list1 = [['Alice',23,3.5,10],['Bob',24,3.4,6],['Charlie',22,3.9,8]]
+df = pd.DataFrame(list1)
+df.columns = ['name','age','cgpa','hoursStudied']
+print(df.head())
