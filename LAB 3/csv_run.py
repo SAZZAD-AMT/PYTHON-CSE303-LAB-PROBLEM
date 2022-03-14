6ec8b61cfcf9c55cfd51ee0f848ccff52844e72a
SAZZAD <sazzad01794@gmail.com>
2022-03-14 20:32:37 +0600

    LAB 3

--- LAB 3/csv_run.py
+++ LAB 3/csv_run.py
@@ -3,7 +3,7 @@ import pandas as pd
 df2=pd.read_csv("sam.csv")
 df2.columns=['id','state','population','murder_rate']
 print(df2)
-print(df2.head() )
+print(df2.head())
 print(df2.tail()) 
 print(df2.count())
 
