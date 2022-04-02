f23464cf672cd238f7958cd0f9c1274241b5f76a
SAZZAD <sazzad01794@gmail.com>
2022-04-03 00:19:43 +0600

    ok

--- LAB 5/lab5.py
+++ LAB 5/lab5.py
@@ -97,7 +97,7 @@ y = np.empty_like(x)
 # Add the vector v to each row of the matrix x with an explicit loop
 for i in range(4):
       y[i, :] = x[i, :] + v
-##Better Solution
+## Better Solution
 x = np.array([[1,2,3], [4,5,6], [7,8,9], [10, 11, 12]])
 v = np.array([1, 0, 1])
 vv = np.tile(v, (4, 1))
