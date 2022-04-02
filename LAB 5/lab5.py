297e8479ff20e0448690cd919fa3dce433c7dc98
SAZZAD <sazzad01794@gmail.com>
2022-04-03 00:19:01 +0600

    lab 5

--- LAB 5/lab5.py
+++ LAB 5/lab5.py
@@ -63,7 +63,7 @@ x = np.array([[1,2],[3,4]], dtype=np.float64)
 y = np.array([[5,6],[7,8]], dtype=np.float64)
 v = np.array([9,10])
 w = np.array([11, 12])
-# Inner product of vectors
+# Inner product ofvectors
 print(v.dot(w))
 print(np.dot(v, w))
 # Matrix / vector product
