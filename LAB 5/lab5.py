902f1d068de70768af77a2474370fd2b2f28b735
SAZZAD <sazzad01794@gmail.com>
2022-04-10 00:51:08 +0600

    md

--- LAB 5/lab5.py
+++ LAB 5/lab5.py
@@ -159,9 +159,9 @@ plt.bar(left, height, tick_label = tick_label, width = 0.8, color =
 plt.xlabel('x - axis')
 # naming the y-axis
 plt.ylabel('y - axis')
-# plot title
+# plot title.
 plt.title('My bar chart!')
-# function to show the plot
+# function to show the plot.
 plt.show()
 
 print("END")
