c42edc1f1bb0438fca985f2093de2a5391c665c2
SAZZAD <sazzad01794@gmail.com>
2022-04-16 16:02:30 +0600

    lab 3

--- LAB 2/8.py
+++ LAB 2/8.py
@@ -1,3 +1,2 @@
-highest={num: max([divisor for divisor in range(1,10) if num % divisor == 0]) 
-         for num in range(1,1001)}
+highest={num: max([divisor for divisor in range(1,10) if num % divisor == 0]) for num in range(1,1001)}
 print(highest)
