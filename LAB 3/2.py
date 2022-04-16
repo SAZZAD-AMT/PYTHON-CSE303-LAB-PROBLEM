69798c5a8ad49b537c47a28e00ec2af370172efb
SAZZAD <sazzad01794@gmail.com>
2022-04-16 16:13:35 +0600

    lab 3

--- LAB 3/2.py
+++ LAB 3/2.py
@@ -1,6 +1,5 @@
 import pandas as pd
-sdata = {'Ohio': 35000, 'Texas': 71000, 'Oregon': 16000, 
- 'Utah': 5000}
+sdata = {'Ohio': 35000, 'Texas': 71000, 'Oregon': 16000, 'Utah': 5000}
 
 obj3 = pd.Series(sdata)
 print(obj3)
--- LAB 3/csv_run.py
+++ LAB 3/csv_run.py
@@ -3,8 +3,8 @@ import pandas as pd
 df2=pd.read_csv("sam.csv")
 df2.columns=['id','state','population','murder_rate']
 print(df2)
-print(df2.head())
-print(df2.tail()) 
+print("Head : \n",df2.head())
+print("TAIL : \n",df2.tail()) 
 print(df2.count())
 
 
